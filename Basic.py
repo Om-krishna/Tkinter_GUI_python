503a6560255a21723c979fdcd618dad22786b6e2
Om Krishna <58508323+Om-krishna@users.noreply.github.com>
2020-06-05 21:42:07 +0530

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 19:50:20 2020
+
+@author: Om krishna
+
+"""
+from tkinter import*
+root = Tk()
+theLabel = Label(root, text="This is too easy")
+theLabel.pack()
+root.mainloop()
+
